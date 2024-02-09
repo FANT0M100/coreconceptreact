44f95cae853a129f8e1423577f4a68b229d5735f
FANT0M100 <giorgie103188@gmail.com>
2024-02-09 14:56:26 +0400

    dinamic output values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import headerImage from "../assets/react-core-concepts.png";
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 const Header = () => {
   return (
     <header>
       <img src={headerImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
